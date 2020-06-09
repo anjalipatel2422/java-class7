a9477f5950eee53fd3d024aa6dfe2d919ef6c58c
anjalipatel2422 <anjalipatel2422@gmail.com>
2020-06-09 21:05:37 +0530

    FamilyDetail Class added

---
+++ src/FamilyDetails.java
@@ -0,0 +1,13 @@
+public class FamilyDetails
+{
+    public static void main(String[]agr){
+        System.out.println("Name-----: kanubhai patel");
+        System.out.println("Address--: 4 - Dhanlaxmi");
+        System.out.println("Opp.-----: I T I Gorwa");
+        System.out.println("Distic---: Vadodara");
+        System.out.println("Pin code-: 390 016");
+        System.out.println("Mo.NO.---: 34567 678590");
+        System.out.println("Age -----: 68");
+        System.out.println("salary---: Retiared");
+
+}}
